91f56ab5c7ab6017625ea6623ce7d1ecfa25d234
Carlos Eduardo <sobralcarlos11@gmail.com>
2022-07-09 17:56:11 -0300

    operadores de condicionais

---
+++ opComparacao/index.js
@@ -0,0 +1,30 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+=== igualdade estrita (valor e tipo)
+!== diferente estrito (valor e tipo)
+*/
+const num1 = 10; // number
+const num2 = 10; // number
+const comp = num1 !== num2;
+console.log(comp);
+
+/*
+Operadores Lógicos
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+// const expressaoAnd = true && true && true && true;
+// const expressaoOr = false || false || false || false;
+
+// const usuario = 'Luiz'; // form usuário digitou
+// const senha = '123456'; // form usuário digitou
+
+// //                      True                  False
+// const vaiLogar = usuario === 'Luiz' && senha === '12345';
+
+console.log(!!true);
